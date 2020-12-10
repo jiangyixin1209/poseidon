d4a63ee87f1374c0e6e4c2d8b182af68e6b444b2
jiangyixin1209 <17602173915@163.com>
2020-12-10 17:58:14 +0800

    LocalCache

--- poseidon-core/src/main/java/top/jiangyixin/poseidon/core/core/LocalCacheConfig.java
+++ poseidon-core/src/main/java/top/jiangyixin/poseidon/core/core/LocalCacheConfig.java
@@ -1,7 +1,60 @@
 package top.jiangyixin.poseidon.core.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import top.jiangyixin.poseidon.core.exception.PoseidonException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author jiangyixin
  */
 public class LocalCacheConfig {
+	private static final Logger logger = LoggerFactory.getLogger(LocalCacheConfig.class);
+
+	private static ConcurrentHashMap<String, String> localCache = null;
+	private static boolean refreshThreadStop = false;
+	private static Thread refreshThread;
+	private static boolean initOk = false;
+
+
+	public static void init() {
+		if (initOk) {
+			throw new PoseidonException("RemoteConfig only can be init once !");
+		}
+		localCache = new ConcurrentHashMap<>();
+
+		Map<String, String> configData = new HashMap<>();
+		Map<String, String> mirrorConfigData = MirrorConfig.readMirrorConfig();
+
+		if (mirrorConfigData != null && mirrorConfigData.size() > 0) {
+			configData.putAll(mirrorConfigData);
+		}
+
+		for (String key : configData.keySet()) {
+
+		}
+
+		initOk = true;
+	}
+
+	public static void destroy() {
+		if (refreshThread != null) {
+			refreshThreadStop = true;
+			refreshThread.interrupt();
+		}
+	}
+	public static void startRefreshThread() {
+		refreshThread = new Thread(() -> {
+			while (!refreshThreadStop) {
+
+			}
+		});
+		refreshThread.setDaemon(true);
+		refreshThread.start();
+		logger.info(">>>>>>>>>> RemoteConfig init success");
+
+	}
 }
--- poseidon-core/src/main/java/top/jiangyixin/poseidon/core/core/MirrorConfig.java
+++ poseidon-core/src/main/java/top/jiangyixin/poseidon/core/core/MirrorConfig.java
@@ -3,7 +3,13 @@ package top.jiangyixin.poseidon.core.core;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import top.jiangyixin.poseidon.core.exception.PoseidonException;
+import top.jiangyixin.poseidon.core.util.PropertyUtils;
 import top.jiangyixin.poseidon.core.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 /**
  * @author jiangyixin
@@ -17,5 +23,37 @@ public class MirrorConfig {
 			throw new PoseidonException("MirrorFile can not be empty");
 		}
 		MirrorConfig.mirrorFile = mirrorFile;
+	}
+
+	/**
+	 * 读取本地镜像缓存配置文件
+	 * @return              Map<String, String>
+	 */
+	public static Map<String, String> readMirrorConfig() {
+		Properties mirrorProperties = PropertyUtils.readProperty(mirrorFile);
+		if (mirrorProperties == null) {
+			return null;
+		}
+		Set<String> propertyKeys = mirrorProperties.stringPropertyNames();
+		if (propertyKeys != null && propertyKeys.size() > 0) {
+			Map<String, String> mirrorConfigData = new HashMap<>(propertyKeys.size());
+			for (String key : propertyKeys) {
+				mirrorConfigData.put(key, mirrorProperties.getProperty(key));
+			}
+			return mirrorConfigData;
+		}
+		return null;
+	}
+
+	/**
+	 * 更新本地镜像缓存配置文件
+	 * @param mirrorConfigData      镜像配置数据
+	 */
+	public static void writeMirrorConfig(Map<String, String> mirrorConfigData) {
+		Properties properties = new Properties();
+		for (Map.Entry<String, String> config : mirrorConfigData.entrySet()) {
+			properties.setProperty(config.getKey(), config.getValue());
+		}
+		PropertyUtils.writePropertyFile(properties, mirrorFile);
 	}
 }
